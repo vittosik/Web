6cde88456421f01d6a0595ca4a62353b4d36fcee
vitosik <viiiitos@gmail.com>
2020-08-10 13:26:18 +0300

    add conf

---
+++ hello.py
@@ -0,0 +1,6 @@
+def wsgi_application(environ, start_response):
+    body = body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+    status = '200 OK'
+    headers = [("Content-Type", "text/plain")]
+    start_response(status, headers)
+    return body
