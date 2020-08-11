8c7c89a258bc73f4b9d024e1e075200725df6d08
vitosik <viiiitos@gmail.com>
2020-08-11 10:29:50 +0300

    change

--- hello.py
+++ hello.py
@@ -1,9 +1,8 @@
 def wsgi_application(environ, start_response):
     status = '200 OK'
-    body = [bytes(i + '\n', 'utf-8') for i in environ['QUERY_STRING'].split('&')]
+    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
     headers = [
-        ("Content-Type", "text/plain"), 
-        ('Content-Length', str(len(body)))
+        ("Content-Type", "text/plain")
         ]    
     start_response(status, headers)
     return body
